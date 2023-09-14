bb1a5fa0ac5fbc311f3846da46f50dbf455da77e
dongjun <adj0707@gmail.com>
2023-09-14 09:43:43 +0900

    feat: adding 2023 players_id

--- data/crawl_boxscore.py
+++ data/crawl_boxscore.py
@@ -12,10 +12,9 @@ formatter = logging.Formatter('|%(asctime)s||%(name)s||%(levelname)s|\n%(message
 if logger.hasHandlers(): ## 핸들러 존재 여부
     logger.handlers.clear() ## 핸들러 삭제
 
-file_handler = logging.FileHandler('boxscore.log') ## 파일 핸들러 생성
+file_handler = logging.FileHandler('logs/boxscore.log') ## 파일 핸들러 생성
 file_handler.setFormatter(formatter) ## 텍스트 포맷 설정
 logger.addHandler(file_handler) ## 핸들러 등록
-
 
 def boxscore_crawl(y):
     for year in range(y, y+1):
@@ -25,15 +24,16 @@ def boxscore_crawl(y):
         
         for match in match_ids:
             game_id = match["game_id"]
-            Event().wait(0.1)
-            boxscore = statsapi.boxscore_data(game_id, timecode=None)
-            input ={"game_id":game_id, "boxscore": boxscore}
-            with open(f"boxscores/boxscore_{game_id}.json", "w") as f:
-                f.write(json.dumps(input))
-            logger.info(f"{game_id} finished")
-        
+            try:
+                boxscore = statsapi.get("game_boxscore", {"gamePk":game_id})
+                input ={"game_id":game_id, "boxscore": boxscore}
+                with open(f"boxscores/boxscore_{game_id}.json", "w") as f:
+                    f.write(json.dumps(input))
+                logger.info(f"{game_id} finished")
+            except Exception as e:
+                logger.error(f"Error for game_id {game_id}: {str(e)}")
 
-for y in range(1901, 2023):
+for y in range(1980, 1984):
     logger.info(f"=====year:{y} started")
     boxscore_crawl(y)
     logger.info(f"=====year:{y} finished")
